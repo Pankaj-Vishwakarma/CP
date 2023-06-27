88d3898bba57dcc9271f1f265e5afb3564c2c65f
Pankaj <pkv.3192@gmail.com>
2023-06-27 18:30:47 +0530

    initial commit

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define file(name) if (fopen(name".INP", "r")){freopen(name".INP", "r", stdin); freopen(name".OUT", "w", stdout);}
+#define all(v) v.begin(),v.end()
+const int maxn = 2e5 + 7;
+const int inf = 1e9 + 7;
+typedef long long ll;
+void solution(){
+
+	int n, id[maxn];
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		int nums; cin >> nums;
+	}
+	
+}
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	file("TEST");
+	
+	int test; cin >> test;
+	while (test --> 0) solution();
+	
+	return 0;
+}
